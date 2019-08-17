ca5daac09e12bd71ff23fc04369a9a0c4cf4d6b2
Ludmilla Aires Negri <ludaires@gmail.com>
2019-08-17 15:39:02 -0400

    Create store, actions and reducers folder.

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import usersReducer from './reducers/users.js'
+import currentUser from './reducers/currentUsers.js'
+
+const reducer = combineReducers({
+    users: usersReducer,
+    currentUser: currentUser
+});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,composeEnhancer(applyMiddleware(thunk)));
+  
+export default store
