ef78b13f8f0acd2307724fae9ca90851fbe8e7b1
Ludmilla Aires Negri <ludaires@gmail.com>
2019-08-24 18:20:10 -0400

    Connect component to the store. Add getCurrentUser action, load the action under componentDidMount.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login.js';
+import { connect } from 'react-redux';
+import { getCurrentUser } from './actions/currentUser.js';
+
 
 class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
 
   render() {
     return (
       <div>
-        Hi from app.js
+        <Login/>        
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, {getCurrentUser})(App);
