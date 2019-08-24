d238ad24e916ecb6d276a32558fdf46b9f755cc8
Ludmilla Aires Negri <ludaires@gmail.com>
2019-08-24 18:18:04 -0400

    add loginForm to the store.

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import usersReducer from './reducers/users.js'
-import currentUser from './reducers/currentUser.js'
+import usersReducer from './reducers/users.js';
+import currentUser from './reducers/currentUser.js';
+import loginForm from './reducers/loginForm.js';
 
 const reducer = combineReducers({
     users: usersReducer,
-    currentUser: currentUser
+    currentUser: currentUser,
+    loginForm
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer,composeEnhancer(applyMiddleware(thunk)));
