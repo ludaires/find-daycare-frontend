742ea285a9efd313f9538171336a7710bc26012d
Ludmilla Aires Negri <ludaires@gmail.com>
2019-08-24 18:17:00 -0400

    add login and get_current_user actions.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,19 +1,52 @@
-//synchrounous action creators
+//synchronous action creators
 export const setCurrentUser = user => {
     return {
         type: "SET_CURRENT_USER",
         user
     }
 }
-//asynchrounous action creators
+//asynchronous action creators
 export const login = credentials => {
     return dispatch => {
         return fetch('http://localhost:3001/api/v1/login', {
+            credentials: "include",
             method: "POST",
             headers: {
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({email: "user@test.com", username: "test", password: "secret"})
+            body: JSON.stringify(credentials)
         })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+                console.log("Sucess, you are logged in")
+            }
+        })
+        .catch(console.log("passei por aqui")) 
+    }
+}
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch('http://localhost:3001/api/v1/get_current_user', {
+              credentials: "include",
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+                console.log("got the user")
+            }
+        })
+        .catch(console.log("something is wrong"))
     }
 }
