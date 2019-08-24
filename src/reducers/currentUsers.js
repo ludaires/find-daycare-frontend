6847d27ddd3e0ecacf3192f6cdba6bee625e9339
Ludmilla Aires Negri <ludaires@gmail.com>
2019-08-24 18:21:31 -0400

    ...

--- src/reducers/currentUsers.js
+++
@@ -1,8 +0,0 @@
-export default (state = null, action) => {
-    switch (action.type) {
-        case "SET_CURRENT_USER":
-            return action.user;
-        default:
-            return state;
-    }
-}
