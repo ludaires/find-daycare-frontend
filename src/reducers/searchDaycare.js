641b4bfde00f434e227078bd85c6e85f44ed79c8
Ludmilla Aires Negri <ludaires@gmail.com>
2019-09-13 13:26:17 -0400

    Remove the console.log after case 'List_daycares_searched'.

--- src/reducers/searchDaycare.js
+++ src/reducers/searchDaycare.js
@@ -4,7 +4,6 @@ const initialState = { daycares: []};
 export default (state = initialState, action) => {
     switch (action.type) {
         case "LIST_DAYCARES_SEARCHED":
-            console.log(action.payload)
             return {...state, daycares: state.daycares.concat(action.payload)}
         default:
             return state
