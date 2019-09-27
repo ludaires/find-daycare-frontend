ec15057d95c83397182620f9da00ebaef0494d7a
Ludmilla Aires Negri <ludaires@gmail.com>
2019-09-27 10:42:24 -0400

    add fetch post request to add new Daycare to database.

---
+++ src/actions/myDaycares.js
@@ -0,0 +1,67 @@
+export const addDaycare = (bussiness) => {
+    return {
+        type: 'ADD_DAYCARE',
+        payload: bussiness
+    }
+}
+
+export const getDaycares = (bussiness) => {
+    return {
+        type: 'GET_DAYCARE',
+        payload: bussiness
+    }
+}
+// getting daycare from the backend
+export const fetchGetDaycare = () => {
+    // let userId = currentUser.id
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/users/1`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(res => res.json())
+        .then(data =>  { 
+            if (data.error) {
+                alert(data.error)
+              } else {
+                const myDaycares = data.attributes.my_daycare
+                dispatch(getDaycares(myDaycares))
+              }
+        })
+        .catch(console.log)
+    }
+}
+
+// adding daycare to the backend
+export const fetchAddDaycare = (bussiness) => {
+    const daycare = {
+        name: bussiness.name,
+        location: bussiness.location.display_address.toString(),
+        rating: bussiness.rating,
+        review_count: bussiness.review_count,
+        yelp_id: bussiness.id,
+        phone: bussiness.display_phone
+    }
+    return dispatch => {
+        const url = 'http://localhost:3001/api/v1/daycares'
+        return fetch(url, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'application/json'
+        },
+            body: JSON.stringify(daycare)
+        }).then(res => res.json())
+        .then(data => {
+            if (data.error){
+                alert(data.error)
+            } else {
+                dispatch(addDaycare(data))
+                alert('Successfully bookmarked');
+            }
+        })
+        .catch(console.log)
+    }
+}
