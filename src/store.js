6c2fa3762609efade715730cacd17c1a094496b3
Ludmilla Aires Negri <ludaires@gmail.com>
2019-09-27 10:44:29 -0400

    add myDaycare to the store.

--- src/store.js
+++ src/store.js
@@ -4,12 +4,14 @@ import usersReducer from './reducers/users.js';
 import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js';
 import daycaresFromYelp from './reducers/searchDaycare.js';
+import myDaycares from './reducers/myDaycares.js';
 
 const reducer = combineReducers({
     users: usersReducer,
     currentUser: currentUser,
     loginForm,
-    daycaresFromYelp
+    daycaresFromYelp,
+    myDaycares
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer,composeEnhancer(applyMiddleware(thunk)));
