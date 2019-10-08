1441ccda1757cbfd7cb7ff4cf1266a9f784ef4a5
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:43:11 -0400

    Import react-router-dom to create the routes, import all component necessary to setup the route.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
-import MainContainer from './components/MainContainer';
+import MainContainer from './components/MainContainer.js';
+import NavBar from './components/NavBar.js';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser.js';
-
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import MyDaycares from './components/MyDaycares.js';
+import SearchDaycare from './components/SearchDaycare.js';
+import Signup from './components/Signup.js';
+import Logout from './components/Logout';
 
 class App extends React.Component {
 
@@ -14,7 +20,15 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <MainContainer/>        
+        <NavBar/>
+        <Switch>
+          <Route exact path='/search' component={SearchDaycare}/>
+          <Route exact path='/login' component={Login}/> 
+          <Route exact path='/logout' component={Logout}/>
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/my-daycares' component={MyDaycares} />
+        </Switch>
+      <MainContainer/>        
       </div>
     );
   }
