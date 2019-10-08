4c387ec7b87c2c6c66260cc875f940c980cd8d0e
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:53:38 -0400

    change GET_DAYCARES case to return only daycare's array.

--- src/reducers/myDaycares.js
+++ src/reducers/myDaycares.js
@@ -3,9 +3,9 @@ const initialState = { daycares: []};
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case "GET_DAYCARE":
+        case "GET_DAYCARES":
             state = {...state, daycares: action.payload}  
-            return state
+            return state.daycares.data.attributes.my_daycares
         case "ADD_DAYCARE":
             state = {...state, daycares: state.daycares.concat(action.payload)}  
             return state
