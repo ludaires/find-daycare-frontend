10ddfa00999831702f131105e7a368cffa986929
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-08 15:44:04 -0400

    Add signupForm to the store. Correct the name for myDaycares variable.

--- src/store.js
+++ src/store.js
@@ -5,13 +5,15 @@ import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js';
 import daycaresFromYelp from './reducers/searchDaycare.js';
 import myDaycares from './reducers/myDaycares.js';
+import signupForm from './reducers/signupForm.js'
 
 const reducer = combineReducers({
     users: usersReducer,
     currentUser: currentUser,
     loginForm,
     daycaresFromYelp,
-    myDaycares
+    myDaycares,
+    signupForm
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer,composeEnhancer(applyMiddleware(thunk)));
