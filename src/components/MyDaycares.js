8b8a666e37b85682a7942c4252424f759ec8c304
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-11 11:04:34 -0400

    Add componentDidMount to get user Daycares.

--- src/components/MyDaycares.js
+++ src/components/MyDaycares.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchGetDaycare } from '../actions/myDaycares.js';
 import DaycareCard from './DaycareCard.js';
+import { fetchGetDaycare } from '../actions/myDaycares.js';
 
 
 class MyDaycares extends React.Component {
 
     componentDidMount(){
-        this.props.fetchGetDaycare()
+        this.props.fetchGetDaycare(this.props.currentUser.id);
     }
-  
+
+
     render(){
         return (
             <div>
                 {this.props.daycares.length > 0 ? this.props.daycares.map(daycare => <DaycareCard key={daycare.id} daycare={daycare}/>) : null }
+                {console.log("Inside MyDaycare the user is:", this.props.currentUser)}
             </div>
         );
     }     
@@ -21,8 +23,9 @@ class MyDaycares extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        daycares: state.myDaycares
+        daycares: state.myDaycares,
+        currentUser: state.currentUser
     }
 }
 
-export default connect(mapStateToProps, {fetchGetDaycare})(MyDaycares);
+export default connect(mapStateToProps, {fetchGetDaycare} )(MyDaycares);
