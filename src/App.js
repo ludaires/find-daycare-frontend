25aaa591ecdeee3fe1313895aa69cc94c0e5a94a
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-18 10:05:39 -0400

    Add new route to home page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import MainContainer from './components/MainContainer.js';
+// import MainContainer from './components/MainContainer.js';
 import NavBar from './components/NavBar.js';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser.js';
 import { Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './components/Login.js';
 import MyDaycares from './components/MyDaycares.js';
 import SearchDaycare from './components/SearchDaycare.js';
 import Signup from './components/Signup.js';
-import Logout from './components/Logout';
+import Logout from './components/Logout.js';
+import Home from './components/Home.js'
 
 class App extends React.Component {
 
@@ -22,13 +23,13 @@ class App extends React.Component {
       <div className="container">
         <NavBar/>
         <Switch>
-          <Route exact path='/search' component={SearchDaycare}/>
+          <Route exact path='/' component={Home}/>  
+          <Route exact path='/search' component={SearchDaycare}/>          
           <Route exact path='/login' component={Login}/> 
           <Route exact path='/logout' component={Logout}/>
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/my-daycares' component={MyDaycares} />
         </Switch>
-      <MainContainer/>        
       </div>
     );
   }
