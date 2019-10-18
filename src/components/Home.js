e501a803e8c2f41e5aef6c08db64fa240352f4ae
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-18 10:05:52 -0400

    Create a home page.

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Find Daycare</h1>
+            <p>
+                Find the perfect place for you kid is not an easy task. 
+                We are here to help you find organizing the search for the best place for you kid.
+            </p>
+        </div>
+    )
+    
+}
+
+
+export default Home;
