96b0931cb71839caf677b20f98538eb2a5d75f49
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-18 10:06:18 -0400

    Add daycareForm

--- src/store.js
+++ src/store.js
@@ -1,20 +1,21 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import usersReducer from './reducers/users.js';
 import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js';
 import daycaresFromYelp from './reducers/searchDaycare.js';
 import myDaycares from './reducers/myDaycares.js';
-import signupForm from './reducers/signupForm.js'
+import signupForm from './reducers/signupForm.js';
+import daycareForm from './reducers/daycareForm.js';
 
 const reducer = combineReducers({
-    users: usersReducer,
-    currentUser: currentUser,
+    currentUser,
     loginForm,
     daycaresFromYelp,
     myDaycares,
-    signupForm
+    signupForm,
+    daycareForm
 });
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer,composeEnhancer(applyMiddleware(thunk)));
   
