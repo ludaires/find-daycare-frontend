7a161999e3a30a531d63ba1bb7dc3b0365b8eb46
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-20 12:26:30 -0400

    Add withRouter and add style.

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions/currentUser.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { withRouter } from 'react-router-dom'
 
-const Logout = ({ logout }) => {
-    return (
-        <form onSubmit={ e => {
-            e.preventDefault();
-            logout()
-        }}>
-            <input type="submit" value="Log Out"/>            
-        </form>
-    )
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+const Logout = ({ logout, history }) => {
+  const classes = useStyles();
+  return (
+    <form onSubmit={ e => {
+        e.preventDefault();
+        logout()
+        history.push('/')
+    }}>
+      <Button 
+          color="secondary" 
+          className={classes.button}
+          type="submit">
+          Logout
+      </Button>    
+    </form>
+  )
 }
 
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout } )(Logout))
