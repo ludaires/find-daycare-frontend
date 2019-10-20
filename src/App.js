01b8b0bc1d74fd4a5102ab54bd76dbe5f55f0852
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-20 11:59:20 -0400

    Add withRouter to use history, change search component to be the home page.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 import NavBar from './components/NavBar.js';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser.js';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import Login from './components/Login.js';
 import MyDaycares from './components/MyDaycares.js';
 import SearchDaycare from './components/SearchDaycare.js';
 import Signup from './components/Signup.js';
 import Logout from './components/Logout.js';
-import Home from './components/Home.js'
 
 class App extends React.Component {
 
@@ -23,16 +22,23 @@ class App extends React.Component {
       <div className="container">
         <NavBar/>
         <Switch>
-          <Route exact path='/' component={Home}/>  
-          <Route exact path='/search' component={SearchDaycare}/>          
+          <Route exact path='/' component={SearchDaycare}/>   
+          <Route exact path='/signup' render={({history}) => <Signup history={history} /> } />
           <Route exact path='/login' component={Login}/> 
           <Route exact path='/logout' component={Logout}/>
-          <Route exact path='/signup' component={Signup} />
           <Route exact path='/my-daycares' component={MyDaycares} />
         </Switch>
       </div>
-    );
+    ); 
   }
 }
 
-export default connect(null, {getCurrentUser})(App);
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
+
+
+
+export default withRouter(connect(mapStateToProps, {getCurrentUser})(App));
