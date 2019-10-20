f25118ac8553b560a508f5e3a7410581225f0a80
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-20 12:16:59 -0400

    Add style using @material-ui.

--- src/components/DaycareCard.js
+++ src/components/DaycareCard.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateDaycareForm, fetchUpdateDaycare } from '../actions/daycareForm.js';
+import { updateDaycareForm, fetchUpdateDaycare, resetDaycareForm } from '../actions/daycareForm.js';
 import { fetchDeleteDaycare } from '../actions/myDaycares.js';
+import Icon from '@mdi/react';
+import { mdiMapMarker } from '@mdi/js';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SaveIcon from '@material-ui/icons/Save';
+// import Icon from '@material-ui/core/Icon';
 
 class DaycareCard extends React.Component {
-   
+
+    useStyles = makeStyles(theme => ({
+        button: {
+          margin: theme.spacing(2),
+        },
+    }));
+
+    classes = () => this.useStyles();
+
     handleInputChange = e => {
         // e.preventDefault();
         const { name, value } = e.target;
@@ -12,8 +27,9 @@ class DaycareCard extends React.Component {
             ...this.props.daycareFormData,
             [name]: value
         }
-        console.log("Inside handle change for form, my daycareformdata is: ", updateDaycareInfo)
+        // console.log("Inside handle change for form, my daycareformdata is: ", updateDaycareInfo)
         this.props.updateDaycareForm(updateDaycareInfo)
+        
     }
 
     handleSubmit = (e) => {
@@ -21,6 +37,8 @@ class DaycareCard extends React.Component {
         const userId = this.props.user.id
         const daycareId = this.props.daycare.id
         this.props.fetchUpdateDaycare(this.props.daycareFormData, userId, daycareId)
+        // this.props.resetDaycareForm()
+
     }
 
     handleDeleteDaycare = (e, daycare, user )  => {
@@ -30,20 +48,34 @@ class DaycareCard extends React.Component {
 
     render() {
         return (
-            <div key={this.props.daycare.id}>
-                <h1>{this.props.daycare.name}</h1>
-                <p>Location: {this.props.daycare.location}</p>
-                <p>Rating: {this.props.daycare.rating} | Phone: {this.props.daycare.phone}</p>              
-            <form onSubmit={this.handleSubmit}>
+            <div key={this.props.daycare.id} className="daycare-card">
+                <h1 className="daycare__name">{this.props.daycare.name}</h1>
+                <h2 className="daycare__rating">Rating: {this.props.daycare.rating}</h2>              
+                <p className="daycare__address"> <Icon className="daycare__icon" path={mdiMapMarker} title="Daycare Address" description={this.props.daycare.location} size={1} color="red"/>Location: {this.props.daycare.location} | Phone: {this.props.daycare.phone}</p>
+            <form onSubmit={this.handleSubmit} className="daycare__review">
                 <label> Notes: 
-                    <input onChange={this.handleInputChange} type="text" placeholder={this.props.daycare.notes} name="notes" value={this.props.daycare.notes}/>
-                </label> <br></br>                     
-                <label> Schedule a Visit
-                    <input onChange={this.handleInputChange} type="text" placeholder={this.props.daycare.schedule_visit} name="scheduleVisit" value={this.props.daycare.schedule_visit}/>
+                    <input className="daycare__notes" onChange={this.handleInputChange} type="text" placeholder={this.props.daycare.notes} name="notes" value={this.props.daycareFormData.notes}/>
+                </label> <br></br>  
+                <br></br>      
+                <label> Schedule a Visit:
+                    <input className="daycare__schedule" onChange={this.handleInputChange} type="text" placeholder={this.props.daycare.schedule_visit} name="scheduleVisit" value={this.props.daycareFormData.schedule_visit}/>
                 </label> <br></br>
-                <input type="submit" value="Save"/> 
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    // className={this.classes.button}
+                    startIcon={<SaveIcon />}>
+                        Save
+                </Button>
+                <Button onClick={(e) => this.handleDeleteDaycare(e, this.props.daycare, this.props.user)}
+                variant="contained"
+                color="secondary"
+                // className={this.classes.button}
+                endIcon={<DeleteIcon />}>
+                    Delete
+                </Button>
             </form> 
-            <button onClick={(e) => this.handleDeleteDaycare(e, this.props.daycare, this.props.user)} >Delete</button>
             </div>
         )
     }
@@ -55,4 +87,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect( mapStateToProps, { fetchDeleteDaycare, updateDaycareForm, fetchUpdateDaycare })(DaycareCard);
+export default connect( mapStateToProps, { fetchDeleteDaycare, updateDaycareForm, fetchUpdateDaycare, resetDaycareForm })(DaycareCard);
