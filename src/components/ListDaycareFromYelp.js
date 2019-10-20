3613946eb18a120d106b31f6395c151497bad349
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-20 12:24:51 -0400

    Add style.

--- src/components/ListDaycareFromYelp.js
+++ src/components/ListDaycareFromYelp.js
@@ -4,6 +4,9 @@ import Reviews from './Reviews.js';
 import Icon from '@mdi/react'
 import { mdiMapMarker } from '@mdi/js';
 import { fetchAddDaycare } from '../actions/myDaycares.js';
+import Button from "@material-ui/core/Button";
+import SaveIcon from '@material-ui/icons/Save';
+// import Icon from '@material-ui/core/Icon';
 
 // THIS COMPONENT DISPLAY THE RESULTS OF DAYCARES' SEARCH
 const ListDaycareFromYelp = (props) => {
@@ -15,19 +18,28 @@ const ListDaycareFromYelp = (props) => {
 
     const renderDaycares = () => {
         return props.daycares.map((daycare) => {
-            return (          
+            return (    
                 <li className="daycare-card" key={daycare.id}>
                     {/* <div className="daycare__information"> */}
-                        <h1 className="daycare__name">{daycare.name}</h1>
-                        <img className="daycare__img" src={daycare.image_url} alt={`daycare: ${daycare.name}`}/>
-                        {/* <div> I have to figure out a way to put the icon close to the address */}
-                            {/* <Icon path={mdiMapMarker} title="Daycare Address" description={daycare.location.display_address} size={1} color="red"/> */}
-                            <p className="daycare__address"> <Icon className="daycare__icon" path={mdiMapMarker} title="Daycare Address" description={daycare.location.display_address} size={1} color="red"/> {daycare.location.address1} {daycare.location.city}, {daycare.location.state} {daycare.location.zip_code}| {daycare.display_phone} </p>
-                        {/* </div> */}
-                        <h2 className="daycare__rating">Rating: {daycare.rating}</h2>  
-                        <Reviews reviews={daycare.reviews}/>
+                    <h1 className="daycare__name">{daycare.name}</h1>
+                    <img className="daycare__img" src={daycare.image_url} alt={`daycare: ${daycare.name}`}/>
+                    {/* <div> I have to figure out a way to put the icon close to the address */}
+                        {/* <Icon path={mdiMapMarker} title="Daycare Address" description={daycare.location.display_address} size={1} color="red"/> */}
+                        <p className="daycare__address"> <Icon className="daycare__icon" path={mdiMapMarker} title="Daycare Address" description={daycare.location.display_address} size={1} color="red"/> {daycare.location.address1} {daycare.location.city}, {daycare.location.state} {daycare.location.zip_code}| {daycare.display_phone} </p>
                     {/* </div> */}
-                    <button onClick={(e) => handleOnClick(e, daycare)}>Add Daycare to my list</button>
+                    <h2 className="daycare__rating">Rating: {daycare.rating}</h2>  
+                    <Reviews reviews={daycare.reviews}/>
+                    {/* </div> */}
+                    <Button onClick={(e) => handleOnClick(e, daycare)}
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        className="daycare__button" 
+                        startIcon={<SaveIcon />}
+                    >
+                        Bookmark
+                    </Button>
+                    {/* <button onClick={(e) => handleOnClick(e, daycare)}>Add Daycare to my list</button> */}
                 </li>  
             );
         })
