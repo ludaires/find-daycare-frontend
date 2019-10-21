bb1b4f70224202cc386e014c02bae58e7ce34e04
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-21 15:14:28 -0400

    Remove varibale key from p tag to div tag.

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -4,8 +4,8 @@ import React from 'react';
  const Reviews = (props) => {
     return props.reviews.map(review => {
             return (
-                <div>
-                    <p key={review.id} className="daycare__review">{review.text}</p> 
+                <div key={review.id}>
+                    <p className="daycare__review">{review.text}</p> 
                     <a className="daycare__link" target="blank" href={review.url}>See full review</a> <br></br>
                 </div>
             )
