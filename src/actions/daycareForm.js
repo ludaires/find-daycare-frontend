81e7b0c33394a9908a01ea82d523dea64b2846f6
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-21 15:11:39 -0400

    Passing new argument daycareNote and add variable today to get the current day.

--- src/actions/daycareForm.js
+++ src/actions/daycareForm.js
@@ -13,9 +13,22 @@ export const resetDaycareForm = () => {
     }
 }
 
-export const fetchUpdateDaycare = (daycareFormData, userId, daycareId) => {
+export const fetchUpdateDaycare = (daycareFormData, daycareNote, userId, daycareId) => {
+   console.log(daycareNote)
+    if (!daycareNote) {
+        daycareNote = " "
+    }
+   
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    
+    
+    console.log("after if,:", daycareNote)
     const sendDaycareData = {
-        notes: daycareFormData.notes,
+        notes: daycareNote + ` |${today} `  + daycareFormData.notes,
         schedule_visit: daycareFormData.scheduleVisit 
     }
     return dispatch => {
