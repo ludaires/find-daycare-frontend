1f8c4753b163b0fbaae4ad41f6d289ff1a2fabf4
Ludmilla Aires Negri <ludaires@gmail.com>
2019-10-21 15:14:03 -0400

    clean code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from "react-router-dom";
-import Logout from './Logout.js';
+// import Logout from './Logout.js';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
